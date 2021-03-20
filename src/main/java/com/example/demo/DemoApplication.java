021590c03bd6dbaf5cce8bf21bb39a342eb4296a
j0schiTadamori <j0schitadamori@gmail.com>
2021-03-20 14:20:26 +0300

    Добавил прямоу доступ к контексту для примера.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,17 +2,22 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
+
+	    //ApplicationContext context = new ClassPathXmlApplicationContext("app-context.xml");
+
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("Hello world!");
 
-		// in app-context var:
-        //    <bean id="product" class="main.java.Product"></bean>
-        //    <context:component-scan base-package="main"/>
+		// In app-context var:
+        // <bean id="product" class="main.java.Product"></bean>
+        // <context:component-scan base-package="main"/>
 	}
 
 }
