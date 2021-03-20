9e37baafa9f094aa7089c74b36bff688cc9022ca
j0schiTadamori <j0schitadamori@gmail.com>
2021-03-20 14:15:51 +0300

    Добавил App-context.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,10 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("Hello world!");
+
+		// in app-context var:
+        //    <bean id="product" class="main.java.Product"></bean>
+        //    <context:component-scan base-package="main"/>
 	}
 
 }
